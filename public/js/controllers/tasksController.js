61424df301bd39232f3ba16e8d5a8553aea413ce
Laura <lg3295@gmail.com>
2016-05-05 19:07:44 -1000

    removes unnecessary lines

--- public/js/controllers/tasksController.js
+++ public/js/controllers/tasksController.js
@@ -4,10 +4,8 @@
   angular.module('app')
   .controller('tasksController', ['$scope', 'TaskService', function($scope, TaskService){
 
-    // $scope.TaskService = TaskService;
     TaskService.getTasks().then(function(response) {
       $scope.tasks = response.data.tasks;
-      console.log(response);
     })
   }]);
 })();
