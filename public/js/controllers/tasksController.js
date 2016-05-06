e4ef0f1bcdcf3edd9f3c9ed518d880ba1d3c1b33
Laura <lg3295@gmail.com>
2016-05-05 15:28:21 -1000

    adds task controller

---
+++ public/js/controllers/tasksController.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function(){
+  angular.module('app')
+  .controller('TodoController', ['$scope', 'TaskService', function($scope, TaskService){
+
+    $scope.TaskService = TaskService;
+
+  }])
+})
