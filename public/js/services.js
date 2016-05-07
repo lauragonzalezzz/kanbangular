39b10512f1665fab994843b070c74ef3daa6b59c
Laura <lg3295@gmail.com>
2016-05-06 16:05:06 -1000

    creates delete task which sends a delete request to the server

--- public/js/services.js
+++ public/js/services.js
@@ -1,15 +1,27 @@
 (function () {
   angular.module('app')
     .service('TaskService', ['$http', function($http){
+
       this.getTasks = function(){
         return $http.get('/api/tasks');
       };
+
       this.addTask = function (task) {
         return $http.post('/api/tasks', task);
       };
+
       this.saveTask = function (task) {
-        console.log('what is task', task);
         return $http.put('/api/tasks', task);
       };
+
+      this.deleteTask = function(task){
+        return $http({
+          url: '/api/tasks',
+          method: 'DELETE',
+          data: task,
+          headers: {"Content-Type": "application/json;charset=utf-8"}
+        });
+      };
+
     }]);
 })();
