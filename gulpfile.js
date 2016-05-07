637065cf852b8af1e40d5346852337c24ae9b26a
Laura <lg3295@gmail.com>
2016-05-06 16:46:51 -1000

    gulp connects to nodemon to run server, compiles sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,27 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var nodemon = require('gulp-nodemon');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
-  browserSync.init({
-      server: "./public"
-  });
-
   gulp.watch("scss/**/*.scss", ['sass']);
-  gulp.watch("public/*.html").on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-  return gulp.src("scss/styles.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("public/css"))
-    .pipe(browserSync.stream());
+
+gulp.task('start', function () {
+  nodemon({
+    script : 'server.js',
+  });
 });
 
-gulp.task('default', ['serve']);
+
+gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest("./public/css"))
+});
+
+gulp.task('default', ['serve', 'start']);
