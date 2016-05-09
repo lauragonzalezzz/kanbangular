aa0e14a68db6617a9c7001f1b9341838ca2020d2
Laura <lg3295@gmail.com>
2016-05-09 10:53:37 -1000

    changes schema userName field to username

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    userName: DataTypes.STRING,
+    username: DataTypes.STRING,
     password: DataTypes.STRING
   }, {
     classMethods: {
